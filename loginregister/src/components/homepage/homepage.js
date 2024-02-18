374561c28ccf2f93caa18e0dd588f01a472ad316
agrawalom711@gmail.com <agrawalom711@gmail.com>
2024-02-18 19:14:42 +0530

    Add axios import and fix onClick event in homepage.js

--- loginregister/src/components/homepage/homepage.js
+++ loginregister/src/components/homepage/homepage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./homepage.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const HomePage = () => {
   const navigate = useNavigate();
   return (
     <div className="homepage">
       <h1>hello Home Page</h1>
 
-      <div className="button" onClick={navigate("/login")}>
+      <div className="button" onClick={() => navigate("/login")}>
         Logout
       </div>
     </div>
